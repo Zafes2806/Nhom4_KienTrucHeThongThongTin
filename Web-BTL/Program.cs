2c0381947add68d894a1fe8a7be2489d90596e2a
Zafes <144411099+Zafes2806@users.noreply.github.com>
2025-04-14 20:27:59 +0700

    Update Program.cs

--- Web-BTL/Program.cs
+++ Web-BTL/Program.cs
@@ -69,5 +69,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DBXemPhimContext>();
-BornData_test.SeedingData(context);
+//BornData_test.SeedingData(context);
 app.Run();
