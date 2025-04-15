6e115cc960f69f4a1c9f11ca5c9f3044a7c86b74
Zafes2806 <manhha280699@gmail.com>
2025-04-15 08:14:01 +0700

    Initial commit

--- Web-BTL/DataAccessLayer/Models/CustomerModel.cs
+++ Web-BTL/DataAccessLayer/Models/CustomerModel.cs
@@ -12,6 +12,7 @@ namespace Web_BTL.DataAccessLayer.Models {
         public int CustomerId { get; set; }
         public virtual ICollection<ReviewModel> Reviews { get; set; } // liên kết đến bảng review
         public virtual int? HistoryListId { get; set; } // khoá phụ bảng History List
+
         public virtual int? FavoriteListId { get; set; } // khoá phụ bảng Favorite List
         public virtual int? WatchListId { get; set; } // khoá phụ bảng Watch List
         public virtual WatchListModel? WatchList { get; set; }
